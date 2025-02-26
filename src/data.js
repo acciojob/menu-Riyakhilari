43951040b4c9e414eb65ee2245e3f985348554c0
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-26 22:04:40 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const menuItems = [
+  { id: 1, title: "Buttermilk Pancakes", category: "Breakfast", price: 15.99, img: "path_to_image" },
+  { id: 2, title: "Diner Double", category: "Lunch", price: 13.99, img: "path_to_image" },
+  { id: 3, title: "Godzilla Milkshake", category: "Shakes", price: 6.99, img: "path_to_image" }
+];
+
+export default menuItems;
